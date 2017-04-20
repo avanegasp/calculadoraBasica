1e30e1c0301b757a4c3082cb66f31099cc66022a
Angie Vanegas <arco527@gmail.com>
2017-04-20 00:07:06 -0500

    Calculator working

--- js/app.js
+++ js/app.js
@@ -51,5 +51,39 @@
       var anterior = document.fo.valores.value;
       document.getElementById("valores").value=anterior+num;
       esto=document.fo.valores.value;
+
+      record = 0;
+      igual = 1;
+      var letraRecord
+      var b = 0;
+      var letra = ""
+
+      for(a=1; a<esto.length;a++){
+        if(esto.charAt(a)=="+" || esto.charAt(a)=="-" || esto.charAt(a)=="*" || esto.charAt(a)=="/" ){
+        igual = igual+1;
+        letra=esto.charAt(a);
+      }else{
+        if(igual>record){record=igual;letraRecord=letra}
+          igual=1
+        }
+        b=a
+      }
+      if(igual>record){
+        record=igual;
+        letraRecord=letra;
+      }
+      if(record>2){
+        var anterior = esto;
+        var nuevoValor =anterior.substring(0, anterior.length-1);
+        document.getElementById("valores").value=nuevoValor;
+        record=0;b=0;igual=1;letra="";letraRecord="";
+      }
     }
+  }
+
+  // realizar las operaciones matematicas
+
+  function calcular(){
+    var resultado = eval(document.fo.valores.value);
+      document.fo.valores.value=resultado;
   }
